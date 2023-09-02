6f19dc687da4f343e6a4ea401613641d60e218e2
kashvigarg <kashvigarg02@gmail.com>
2023-09-02 13:59:23 +0530

    added file upload + view pdf

--- doctopdf/pdfapp/views.py
+++ doctopdf/pdfapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, FileResponse
+from django.core.files.storage import FileSystemStorage
+from docx2pdf import convert
+
 def index(request):
-    return HttpResponse("Welcome")
+    if (request.method == 'POST'):
+        req_file = request.FILES['file']
+        old_file_name = req_file.name
+        fs = FileSystemStorage()
+
+        filedata = fs.save(old_file_name,req_file)
+        convert('convfiles/static/'+old_file_name)
+        
+
+        newfileroot = old_file_name.split('.')
+        newfilename = newfileroot[0]+'.pdf'
+        newf = open('convfiles/static/'+newfilename, 'rb')
+        res = FileResponse(newf)
+
+        return res
+    return render(request, 'index.html')
